5b925a1eb4e0186850e35f4f0a01a3c88275b761
coaxial <c@64b.it>
2022-10-25 14:49:31 +0200

    Show upload and docs on homepage

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class SiteController < ApplicationController
-  def home; end
+  def home
+    @document_upload = Document.new
+    @documents = Document.all
+  end
 end
