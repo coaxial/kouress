d93514671f973d80d2344343d86500b2eddf93e2
coaxial <c@64b.it>
2022-11-01 13:30:25 +0100

    Enable Sidekiq UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  # TODO: allow this for the admin in all envs
+  mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
   get 'home', to: 'site#home', as: 'home'
   get 'new_user', to: 'users#new', as: 'new_user'
   get 'login', to: 'sessions#new', as: 'login'
