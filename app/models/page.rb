034648fb078c372fde9c671a8d4d4973fd936c1f
coaxial <c@64b.it>
2022-11-07 14:05:52 +0100

    Extract text when new page created

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,4 +4,11 @@ class Page < ApplicationRecord
   belongs_to :document
   has_one_attached :image
   default_scope { order(:page_num) }
+  after_commit :extract_text, on: :create
+
+  private
+
+  def extract_text
+    TextExtractionJob.perform_later(id)
+  end
 end
