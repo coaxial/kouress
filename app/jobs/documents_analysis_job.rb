f74babae5f3215cf0408f59710ac60ff1b12f2ad
coaxial <c@64b.it>
2022-11-16 11:08:01 +0100

    Fail document when appropriate

--- app/jobs/documents_analysis_job.rb
+++ app/jobs/documents_analysis_job.rb
@@ -20,13 +20,24 @@ class DocumentsAnalysisJob < ApplicationJob
     document.paginate
   end
 
+  def document_path
+    ActiveStorage::Blob.service.path_for(document.file.key)
+  end
+
+  def handle_job_failure(step, context = nil)
+    document.fail
+    msg = "error running #{step}"
+    msg << ": #{context.inspect}" if context
+
+    raise msg
+  end
+
   def create_pages
-    @document_path = ActiveStorage::Blob.service.path_for(document.file.key)
     reader = PDF::Reader.new(document_path)
 
     reader.pages.each do |pdf_page|
       page = Page.new(document:, page_num: pdf_page.number, text: nil)
-      page.save
+      handle_job_failure('analysis', { document:, page: }) unless page.save
     end
   end
 end
