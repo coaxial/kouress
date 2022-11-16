b33b6758c5d23e99a31cc621ab79ce24ff4eb449
coaxial <c@64b.it>
2022-11-16 15:48:54 +0100

    Fix foreign key constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_13_105309) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_16_144025) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,7 +89,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_13_105309) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "document_processing_events", "documents"
-  add_foreign_key "page_processing_events", "pages"
-  add_foreign_key "pages", "documents"
+  add_foreign_key "document_processing_events", "documents", on_delete: :cascade
+  add_foreign_key "page_processing_events", "pages", on_delete: :cascade
+  add_foreign_key "pages", "documents", on_delete: :cascade
 end
