dfb11cbe455b338bfff4d7868dc592cf4bbea3d9
coaxial <c@64b.it>
2022-11-23 11:04:37 +0100

    Rubocop disable line only

--- app/jobs/update_state_for_page_job.rb
+++ app/jobs/update_state_for_page_job.rb
@@ -22,8 +22,7 @@ class UpdateStateForPageJob < ApplicationJob
   def update_document_state
     # #update_counters is required to avoid a race condition. Validations are
     # skipped, but it's fine as this is only a counter we're incrementing.
-    # rubocop:disable Rails/SkipsModelValidations
-    Document.where(id: document.id).update_counters(processed_pages_count: 1, touch: true)
+    Document.where(id: document.id).update_counters(processed_pages_count: 1, touch: true) # rubocop:disable Rails/SkipsModelValidations
     # #update_counters doesn't trigger any ActiveRecord::Callbacks so check
     # here if all pages in the document have been processed and update
     # document's state accordingly.
