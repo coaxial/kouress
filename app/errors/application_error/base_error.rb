b3bfa277d261dfd6668ed51135f73dc2ef8cfb90
coaxial <c@64b.it>
2022-11-23 10:36:11 +0100

    Add doc

--- app/errors/application_error/base_error.rb
+++ app/errors/application_error/base_error.rb
@@ -6,17 +6,23 @@ module ApplicationError
   class BaseError < StandardError
     attr_accessor :context
 
-    def initialize(args = {})
-      @context = args[:context]
+    # @param opts [Hash] the options to instantiate the error.
+    # @option opts [Hash] extra context about the error, such as the models
+    # involved, a system command output, etc.
+    def initialize(opts = {})
+      @context = opts[:context]
       super
     end
 
+    # @return [String] the message for that error, as defined in
+    # config/locales/errors.en.yml
     def message
       I18n.t("error.#{error_name.underscore}.message")
     end
 
     private
 
+    # @return [String] the error's class name without its module (i.e. ApplicationError::SomeFailure => "SomeFailure"
     def error_name
       self.class.name.demodulize
     end
