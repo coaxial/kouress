21419f8f0ca2d1d0d88c2628c9a49487d129b486
coaxial <c@64b.it>
2022-11-29 10:32:16 +0100

    Add tests for Language

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -3,5 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe Language, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:language) { create :language }
+
+  describe '#to_s' do
+    it 'returns the iso_code' do
+      expect(language.to_s).to eq(language.iso_code)
+    end
+  end
+
+  describe '#fulltext_name' do
+    it 'returns the english name' do
+      english_name = ISO_639.find(language.iso_code).english_name
+      expect(language.fulltext_name).to eq(english_name)
+    end
+  end
 end
