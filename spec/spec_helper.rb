414de14c3a77688ec54933b6e27f1c1410148a68
Rock <coaxial@users.noreply.github.com>
2022-11-30 15:25:08 +0100

    Disable code coverage comments (#32)
    Reports are broken until this is merged:
    https://github.com/fortissimo1997/simplecov-lcov/pull/28

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-require 'simplecov-lcov'
-SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
-SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.start
 require 'capybara/rspec'
 require './spec/support/feature_helper'
